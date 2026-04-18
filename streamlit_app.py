e16cd3dc50251bb730595f74ebaf089505c2088e
Sunnykwak7777 <whitehairwitch@naver.com>
2026-04-18 20:37:36 +0900

    try again streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ import numpy as np
 import altair as alt
 import pandas as pd
 import streamlit as st
-
+from datetime import time, datetime
 st.header('st.write')
 st.write('Hello, *World!* :sunglasses:')
 st.write(1234)
@@ -26,3 +26,28 @@ c = alt.Chart(df2).mark_circle().encode(
     x='a', y='b', size='c', color = 'c', tooltip=['a','b', 'c'])
 
 st.write(c)
+
+st.header('st.slider')
+st.subheader('Slider')
+age = st.slider('당신의 나이는?', 0, 130, 25)
+st.write("나는  ", age, '살입니다.')
+
+st.subheader('범위슬라이더')
+values = st.slider(
+    '값의 범위를 선택하세요',
+    0.0, 100.0, (25.0, 75.0))
+st.write('값:', values)
+
+st.subheader('시간범위 슬라이더')
+appointment = st.slider(
+    "약속을 예약하세요:",
+    value = (time(11,30), time(12,45)))
+st.write("예약된 시간:", appointment)
+
+st.subheader('ㄴㄹ짜 및 시간 슬라이더')
+start_time = st.slider(
+    "언제 시작하시겠습니까?",
+    value = datetime(2020, 1,1,9,30),
+    format = "MM/DD/YY - hh:mm")
+st.write("시작시간:", start_time)
+                               
