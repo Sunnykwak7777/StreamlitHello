4832d23c2348ec08618c689204c7b79e353f3f46
Sunnykwak7777 <whitehairwitch@naver.com>
2026-04-21 22:32:06 +0900

    sunglasses 추가

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import altair as alt
 import pandas as pd
 import streamlit as st
 from datetime import time, datetime
+import time
 
 st.set_page_config(layout = "wide")
 st.title("Streamlit 앱 레이아웃 구성하기")
@@ -157,3 +158,14 @@ if uploaded_file is not None:
     st.write(df.describe())
 else:
     st.info('CSV파일을 업로드하세요')
+
+st.title('st.progress')
+with st.expander('이 앱에 대하여'):
+    st.write("'st.progress'명령어를 사용하여 Streamlit앱에서 계산의 진행 상태를 표시할 수 있습니다.")
+my_bar = st.progress(0)
+for percent_complete in range(100):
+    time.sleep(0.05)
+    my_bar.progress(percent_complete+1)
+
+st.balloons()
+
