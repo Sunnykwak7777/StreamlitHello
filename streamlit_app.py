85f07c94b947e7cf286f1888e5be9a0f86e14482
Sunnykwak7777 <whitehairwitch@naver.com>
2026-04-22 23:17:37 +0900

    slow_function추가

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,11 +16,18 @@ def load_data_a():
     columns=['a', 'b', 'c', 'd', 'e']
   )
   return df
+@st.cache_data()
+def slow_function(x):
+  time.sleep(3)  #시간이 오래 걸리는 작업
+  return x*10
+
 
 a0 = time.time()
 st.write(load_data_a())
 a1 = time.time()
 st.info(a1-a0)
+result = slow_function(5)
+st.write("결과:", result)
 
 # 캐시 미사용
 st.subheader('st.cache 미사용')
