27959be8c35f33029609d81fe52f0bfd268f1dff
Sunnykwak7777 <whitehairwitch@naver.com>
2026-04-25 05:51:07 +0900

    read_csv

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,25 +4,18 @@ import pandas as pd
 import streamlit as st
 import time
 
+
+csv_url = "https://raw.githubusercontent.com/Sunnykwak7777/StreamlitHello/main/my__data.csv"
 st.title('st.cache_data')
 
 # 캐시 사용
 st.subheader('st.cache_data 사용')
 
 @st.cache_data()
-def load_data_a():
-  df = pd.DataFrame(
-    np.random.rand(2000000, 5),
-    columns=['a', 'b', 'c', 'd', 'e']
-  )
-  return df
-@st.cache_data()
-def slow_function_a(x):
-  time.sleep(3)  #시간이 오래 걸리는 작업
-  return x*10
-@st.cache_data()
-def load_data():
-  return pd.read_csv("my_data.csv")
+def load_data_a(url):
+    # pandas를 이용해 csv 파일을 읽어옵니다.
+    df = pd.read_csv(url)
+    return df
 
 a0 = time.time()
 st.write(load_data_a())
@@ -36,23 +29,12 @@ st.write(df)
 # 캐시 미사용
 st.subheader('st.cache 미사용')
 
-def load_data_b():
-  df = pd.DataFrame(
-    np.random.rand(2000000, 5),
-    columns=['a', 'b', 'c', 'd', 'e']
-  )
-  return df
-
-def slow_function_b(x):
-  time.sleep(3)  #시간이 오래 걸리는 작업
-  return x*10
+def load_data_b(url):
+    # pandas를 이용해 csv 파일을 읽어옵니다.
+    df = pd.read_csv(url)
+    return df
 
 b0 = time.time()
 st.write(load_data_b())
 b1 = time.time()
 st.info(b1-b0)
-
-result = slow_function_a(15)
-st.write("결과a:", result)
-result = slow_function_b(15)
-st.write("결과b:", result)
