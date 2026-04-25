d8a301168bd50799c20a33db09b33198c1390544
Sunnykwak7777 <whitehairwitch@naver.com>
2026-04-25 13:58:14 +0900

    session_state

--- streamlit_app.py
+++ streamlit_app.py
@@ -136,3 +136,22 @@ def get_connection():
 conn = get_connection()
 df = pd.read_sql("SELECT * FROM users", conn)
 st.dataframe(df)
+
+# session_state 참고파일
+
+st.title('st.session_state')
+
+def lbs_to_kg():
+  st.session_state.kg = st.session_state.lbs/2.2046
+def kg_to_lbs():
+  st.session_state.lbs = st.session_state.kg*2.2046
+
+st.header('입력')
+col1, spacer, col2 = st.columns([2,1,2])
+with col1:
+  pounds = st.number_input("파운드:", key = "lbs", on_change = lbs_to_kg)
+with col2:
+  kilogram = st.number_input("킬로그램:", key = "kg", on_change = kg_to_lbs)
+
+st.header('출력')
+st.write("st.session_state 객체:", st.session_state)
